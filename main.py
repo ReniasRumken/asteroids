ff2127638e986bee5b9aee359cfdfec4631f66ba
Lazy Lizard <tobiasruehl@web.de>
2025-03-07 14:18:40 +0100

    main.py fps added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
